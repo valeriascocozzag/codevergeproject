f0a4aaa456b001e449d3bfd8f40970dad8359804
Valeria Scocozza <valeriascocozzag@gmail.com>
2025-04-01 17:35:45 +0000

    nueva edicion

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ document.getElementById('form-contacto').addEventListener('submit', function (e)
     // Reiniciar el formulario
     this.reset();
   });
+
+  // Mostrar elementos al hacer scroll 
+  const faders = document.querySelectorAll('.fade-in');
+const aparecer = () => {
+  faders.forEach(section => {
+    const pos = section.getBoundingClientRect().top;
+    const screen = window.innerHeight;
+    if (pos < screen - 100) {
+      section.classList.add('visible');
+    }
+  });
+};
+window.addEventListener('scroll', aparecer);
+window.addEventListener('load', aparecer); //
